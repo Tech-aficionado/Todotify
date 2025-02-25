e8550756948c45a1c3791ea85411e618275f3fc3
Shivansh Goel <shivansh.goela12@gmail.com>
2025-02-26 02:24:15 +0530

    init

---
+++ src/app/pages/landing/landing.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { TopbarWidget } from './components/topbarwidget.component';
+import { HeroWidget } from './components/herowidget';
+import { FeaturesWidget } from './components/featureswidget';
+import { HighlightsWidget } from './components/highlightswidget';
+import { PricingWidget } from './components/pricingwidget';
+import { FooterWidget } from './components/footerwidget';
+import { AppFloatingConfigurator } from '../../layout/component/app.floatingconfigurator';
+import { AppConfigurator } from '../../layout/component/app.configurator';
+
+@Component({
+    selector: 'app-landing',
+    standalone: true,
+    imports: [RouterModule,AppConfigurator, TopbarWidget, HeroWidget, FeaturesWidget, HighlightsWidget, PricingWidget, FooterWidget, RippleModule, StyleClassModule, ButtonModule, DividerModule],
+    template: `
+        <app-configurator />
+        <div class="bg-surface-0 dark:bg-surface-900">
+            <div id="home" class="landing-wrapper overflow-hidden">
+                <topbar-widget class="py-6 px-6 mx-0 md:mx-12 lg:mx-20 lg:px-20 flex items-center justify-between relative lg:static" />
+                <hero-widget />
+                <features-widget />
+                <highlights-widget />
+                <pricing-widget />
+                <footer-widget />
+            </div>
+        </div>
+    `
+})
+export class Landing {}
---
+++ src/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn, CanLoad, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Location } from '@angular/common';
+
+export const AuthenticationGaurds: CanActivateFn = (route: any, state: any) => {
+  const router = inject(Router)
+  // const token = localStorage.getItem('Access_token') ?? false;
+  // if (token == false) {
+    router.navigate(['auth/access']);
+  //   return false;
+  // }
+  return false;
+};
