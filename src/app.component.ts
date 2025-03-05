8bcf14d4e3d8c87d5dcef81bff211c88fbb317c7
Shivansh Goel <shivansh.goela12@gmail.com>
2025-03-05 22:11:00 +0530

    wip

--- src/app.component.ts
+++ src/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
     async ngOnInit() {
       // Set status bar style and visibility
       await StatusBar.setStyle({ style: StatusBarStyle.Light }); // Light text on dark background
-      await StatusBar.show(); // Ensure status bar is visible
+      await StatusBar.hide(); // Ensure status bar is visible
       await StatusBar.setBackgroundColor({ color: '#2c3e50' }); // Match your app’s background color
     }
   }
